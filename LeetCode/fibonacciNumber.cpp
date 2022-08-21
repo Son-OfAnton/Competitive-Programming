ac63ef97645a7c850f4ab754806c1b84a4998a1d
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-08-21 16:04:46 +0300

    Update fibonacciNumber.cpp

--- LeetCode/fibonacciNumber.cpp
+++ LeetCode/fibonacciNumber.cpp
@@ -9,3 +9,25 @@ public:
             return fib(n-1) + fib(n-2);
     }
 };
+
+/*
+Better implementation in java
+
+class Solution {
+    public int fib(int n) {
+        if(n == 1 || n == 0){
+            return n;
+        }
+        else{
+            int[] arr = new int[n+1];
+            arr[0] = 0;
+            arr[1] = 1;
+            for(int i=2; i<=n; i++){
+                arr[i] += arr[i-1] + arr[i-2];
+            } 
+        return arr[n];
+        }
+    }
+}
+
+*/
