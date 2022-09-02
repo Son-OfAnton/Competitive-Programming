57d02931655052af52e8f7c9163b5123bc588d3c
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-02 13:03:00 +0300

    Update carFleet.py

--- LeetCode/carFleet.py
+++ LeetCode/carFleet.py
@@ -3,15 +3,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         stack = []
-        result = 0
+        numberOfFleets = 0
         
         positionSpeed = [(position[i],speed[i]) for i in range(len(position))]
         positionSpeed.sort(reverse=True)
         
-        for x,v in positionSpeed:
-            a = (target-x)/v
-            if len(stack) == 0 or a > stack[-1]:
-                stack.append(a)
-                result += 1
+        for position,speed in positionSpeed:
+            time = (target - position) / speed
+            if len(stack) == 0 or time > stack[-1]:
+                stack.append(time)
+                numberOfFleets += 1
             
-        return result
+        return numberOfFleets
