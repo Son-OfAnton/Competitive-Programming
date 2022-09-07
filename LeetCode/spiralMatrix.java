f749d8414ff1cd928968a089b1c60183a74bfb8e
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-08 01:26:15 +0300

    Update spiralMatrix.java

--- LeetCode/spiralMatrix.java
+++ LeetCode/spiralMatrix.java
@@ -3,43 +3,46 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         
-        List<Integer> list = new ArrayList<>();
+        List<Integer> spiral = new ArrayList<>();
         
-        if(matrix.length == 0 || matrix[0].length == 0) return list;
+        if(matrix.length == 0 || matrix[0].length == 0) 
+            return spiral;
         
-        int rowBegin = 0; 
-        int rowEnd = matrix.length -1;
-        int colBegin = 0;
-        int colEnd = matrix[0].length -1;
+        int rowBegin = 0, colBegin = 0; 
+        int rowEnd = matrix.length - 1;
+        int colEnd = matrix[0].length - 1;
         
         
         while(rowBegin <= rowEnd && colBegin <= colEnd){
             
-            for(int i = colBegin; i <= colEnd; i++){
-                list.add(matrix[rowBegin][i]);
+            for(int i = colBegin; i <= colEnd; i++) {
+                spiral.add(matrix[rowBegin][i]);
             }
             rowBegin++;
             
-            for(int i = rowBegin; i <= rowEnd; i++){
-                list.add(matrix[i][colEnd]);
+            for(int i = rowBegin; i <= rowEnd; i++) {
+                spiral.add(matrix[i][colEnd]);
             }
             colEnd--;
             
-            if(rowBegin <= rowEnd){
-                for(int i = colEnd; i >= colBegin; i--){
-                    list.add(matrix[rowEnd][i]);
+            if(rowBegin <= rowEnd) {
+                for(int i = colEnd; i >= colBegin; i--) {
+                    spiral.add(matrix[rowEnd][i]);
                 }
             }
             rowEnd--;
             
 
-            if(colBegin <= colEnd){
-                for(int i = rowEnd; i >= rowBegin; i--){
-                    list.add(matrix[i][colBegin]);
+            if(colBegin <= colEnd) {
+                for(int i = rowEnd; i >= rowBegin; i--) {
+                    spiral.add(matrix[i][colBegin]);
                 }
             }
             colBegin++;
         }
-        return list;
+        return spiral;
     }
 }
+
+
+      
