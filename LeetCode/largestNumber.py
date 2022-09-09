864166c3700d84854683fc89c3e5b7126e5a4135
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-10 02:52:32 +0300

    Update largestNumber.py

--- LeetCode/largestNumber.py
+++ LeetCode/largestNumber.py
@@ -4,11 +4,16 @@ class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         nums = [str(i) for i in nums]
         
-        def custom_sort(a, b):
+        # a custom function to compare based on their permutation
+        def custom_sort(a,b):
             if a + b > b + a:
                 return -1
             else:
                 return 1
         nums = sorted(nums, key = cmp_to_key(custom_sort))
         
-        return str(int("".join(nums)))
+        # removing leading zeroes
+        while nums[0] == '0' and len(nums) > 1:
+            nums.pop(0)
+        
+        return "".join(nums)
