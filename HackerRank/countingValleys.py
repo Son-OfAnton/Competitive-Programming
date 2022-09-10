15ac1b4c2f82c1285cfd21ee1d232c455c762944
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-11 02:20:22 +0300

    changed variable names

--- HackerRank/countingValleys.py
+++ HackerRank/countingValleys.py
@@ -1,11 +1,11 @@
 def countingValleys(steps, path):
     # Write your code here
-    alt, valleys = 0, 0
+    altitute, valleys = 0, 0
     for i in path:
         if i == 'U':
-            if alt == -1:
+            if altitute == -1:
                 valleys += 1
-            alt += 1
+            altitute += 1
         else:
-            alt -= 1
+            altitute -= 1
     return valleys
