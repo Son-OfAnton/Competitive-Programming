442411a456a3d350becbf6f792348cbd8e80a3b4
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-12 00:01:15 +0300

    Create  findTheKthLargestIntegerInTheArray.py

---
+++ LeetCode/findTheKthLargestIntegerInTheArray.py
@@ -0,0 +1,7 @@
+# 1985.Find the Kth Largest Integer in the Array
+# naive solution
+
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        nums.sort(key=int)
+        return nums[-k]
