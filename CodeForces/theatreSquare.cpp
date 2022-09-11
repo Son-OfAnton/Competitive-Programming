b3a18601774cc90860119f0fe39087b0fd4ab862
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-11 21:52:40 +0300

    Create theatreSquare.cpp

---
+++ CodeForces/theatreSquare.cpp
@@ -0,0 +1,11 @@
+// Theatre Square - 1A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double n, m, a;
+    cin >> n >> m >> a;
+    cout << (long long)ceil(n / a) * (long long)ceil(m / a) << endl;
+}
