850ef36f09ef8fffc79b9fdb32aa631a0df10eb6
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-18 11:44:15 +0300

    Update rangeSumQuery2D.py

--- LeetCode/rangeSumQuery2D.py
+++ LeetCode/rangeSumQuery2D.py
@@ -3,19 +3,32 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.prefix = []
-        for i in range(len(matrix)):
-            self.prefix.append([0])
-            for j in range(len(matrix[i])):
-                self.prefix[i].append(matrix[i][j] + self.prefix[i][-1])
+        
+        # horizontal prefixsum 
+        for row in range(len(matrix)):
+            for col in range(1, len(matrix[0])):
+                matrix[row][col] += matrix[row][col - 1]
+                
+                
+        # vertical prefixsum
+        p = 0
+        while p < len(matrix[0]):
+            for row in range(1, len(matrix)):
+                matrix[row][p] += matrix[row - 1][p]
+            p += 1
+        self.matrix = matrix
+            
+        
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        self.sum = 0
-        for row in range(row1, row2+1):
-            self.sum += self.prefix[row][col2+1] - self.prefix[row][col1]
-        
-        return self.sum
-
+        answer = self.matrix[row2][col2]
+        if col1 - 1 >= 0: 
+            answer -= self.matrix[row2][col1 - 1]
+        if row1 - 1 >= 0:
+            answer -= self.matrix[row1 - 1][col2]
+        if row1 - 1 >= 0 and col1 - 1 >= 0:
+            answer += self.matrix[row1 - 1][col1 - 1]
+        return answer
 
 
 # Your NumMatrix object will be instantiated and called as such:
