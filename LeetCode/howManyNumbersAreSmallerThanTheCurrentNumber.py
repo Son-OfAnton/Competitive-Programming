32b27bcc34a7fcec625ca6976e468ac7f867c17e
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-09-25 19:02:39 +0300

    Update howManyNumbersAreSmallerThanTheCurrentNumber.py

--- LeetCode/howManyNumbersAreSmallerThanTheCurrentNumber.py
+++ LeetCode/howManyNumbersAreSmallerThanTheCurrentNumber.py
@@ -2,9 +2,10 @@
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        res = []
         sorted_nums = sorted(nums)
-        for i in nums:
-            res.append(sorted_nums.index(i))
         
-        return res
+        for i, num in enumerate(nums):
+            nums[i] = sorted_nums.index(num)
+        
+        return nums
+
