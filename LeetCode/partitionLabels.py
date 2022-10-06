68dc62e0a62b667748df4822b40eea2b0626b3fc
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-06 12:20:06 +0300

    Update partitionLabels.py

--- LeetCode/partitionLabels.py
+++ LeetCode/partitionLabels.py
@@ -19,3 +19,30 @@ class Solution:
 # the above condition we will append the length of the substring between 
 # the pointers to the res list and place the left pointer at the front of the 
 # right one.
+
+# A better solution
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_seen = {}
+        res = []
+             
+        for i, alpha in enumerate(s):
+            last_seen[alpha] = i
+            left, right = 0, -1
+
+        for i, alpha in enumerate(s):
+            if last_seen[alpha] > right:
+                right = last_seen[alpha]
+            if i == right:
+                res.append(right - left + 1)
+                left = right + 1
+        
+        return res
+    
+# First we make a dictionary of alphabets. Then we place the right pointer 
+# at the last seen index of an alphabet while we iterate over the string as 
+# long as the last seen index of an alphabet is greater than the right pointer.
+# When the index of an alphabet is equal to the right index, we can be sure that
+# the substring between the two pointer is valid then we append its length to res.
+
+
