a5164841e32d3fecc602b590218e8cd03b4909e7
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-09 22:15:52 +0300

    Update findAllAnagramsInAString.py

--- LeetCode/findAllAnagramsInAString.py
+++ LeetCode/findAllAnagramsInAString.py
@@ -32,3 +32,12 @@ class Solution:
                 res.append(i - len(p) + 1)
                 
         return res
+
+# First we make a list which contains the number of each expected
+# letter in our window. Then we start sliding our window on s. Then
+# we store the new letter in our window in in_idx then we check that
+# if it a letter we want if it is we decrement the number of wanted 
+# letters(diff). Then we check if our window exceeded the length of p
+# by one or more, if it exceedes we remove the leftmost letter from 
+# the window and mark it as unseen. If diff == 0 we are sure that the 
+# current window contains an anagram.
