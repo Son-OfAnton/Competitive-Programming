fa8e924054d87caf1a70608679e0f8fe6d3c7b42
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-12 16:09:32 +0300

    Update longestSubarrayOf1sAfterDeletingOneElement.py

--- LeetCode/longestSubarrayOf1sAfterDeletingOneElement.py
+++ LeetCode/longestSubarrayOf1sAfterDeletingOneElement.py
@@ -2,20 +2,28 @@
 
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        longest = 0
-        left = 0
-        zero_count = 0
+            longest = 0
+            left = 0
+            zero_count = 0
 
-        for right in range(len(nums)):
-            if nums[right] == 0: 
-                zero_count += 1
+            for right in range(len(nums)):
+                if nums[right] == 0: 
+                    zero_count += 1
 
-            if zero_count > 1:
                 while zero_count > 1:
                     if nums[left] == 0: 
                         zero_count -= 1
                     left += 1
 
-            longest = max(longest, right - left + 1)
+                longest = max(longest, right - left)
 
-        return longest - 1
+            return longest
+        
+# This question is similar to 1004. Max Consecutive Ones III
+# in this case there is deleting instead of flipping. And the 
+# question can be rephrased as the longes sequence with only one
+# 0. So what we are going  to do is moving the right bound of 
+# the window until we reach two zeros in the window right exclusive. 
+# Then begin moving the left bound until we clear enough zeroes from 
+# the window.
+
