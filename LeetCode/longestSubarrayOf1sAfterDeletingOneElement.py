ae28c0453ba973091ec0cd668c398fc98529317f
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-12 15:01:17 +0300

    Update longestSubarrayOf1sAfterDeletingOneElement.py

--- LeetCode/longestSubarrayOf1sAfterDeletingOneElement.py
+++ LeetCode/longestSubarrayOf1sAfterDeletingOneElement.py
@@ -2,20 +2,20 @@
 
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-            longest = 0
-            left = 0
-            zero_count = 0
+        longest = 0
+        left = 0
+        zero_count = 0
 
-            for right in range(len(nums)):
-                if nums[right] == 0: 
-                    zero_count += 1
+        for right in range(len(nums)):
+            if nums[right] == 0: 
+                zero_count += 1
 
-                if zero_count > 1:
-                    while zero_count > 1:
-                        if nums[left] == 0: 
-                            zero_count -= 1
-                        left += 1
+            if zero_count > 1:
+                while zero_count > 1:
+                    if nums[left] == 0: 
+                        zero_count -= 1
+                    left += 1
 
-                longest = max(longest, right - left + 1)
+            longest = max(longest, right - left + 1)
 
-            return longest - 1
+        return longest - 1
