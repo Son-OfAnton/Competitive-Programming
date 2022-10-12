cccdec8e7f95479bc5c1a6b6067baa28cf605eed
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-12 15:23:33 +0300

    Update maxConsecutiveOnesIII.py

--- LeetCode/maxConsecutiveOnesIII.py
+++ LeetCode/maxConsecutiveOnesIII.py
@@ -2,7 +2,7 @@
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        left = right = 0
+        left = 0
         
         for right in range(len(nums)) :
             if nums[right] == 0:
@@ -12,7 +12,6 @@ class Solution:
                 if nums[left] == 0:
                     k += 1
                 left += 1
-            
                 
         return len(nums) - left
 
