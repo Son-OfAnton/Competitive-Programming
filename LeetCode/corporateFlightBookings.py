c4494256289a3dd972926bfce7b7d5cbbe57cff8
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-13 22:06:15 +0300

    Update corporateFlightBookings.py

--- LeetCode/corporateFlightBookings.py
+++ LeetCode/corporateFlightBookings.py
@@ -2,13 +2,33 @@
 
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        res = [0] * n
-        for first, last, seat in bookings:
-            res[first - 1] += seat
+        answer = [0] * n
+        for booking in bookings:
+            first, last, seat = booking[0], booking[1], booking[2]
+            answer[first - 1] += seat
+            
             if last < n:
-                res[last] -= seat
+                answer[last] -= seat
             
         for i in range(1, n):
-            res[i] += res[i - 1]
+            answer[i] += answer[i - 1]
         
-        return res
+        return answer
+      
+      
+# A naive java solution
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+
+#  class Solution {
+#     public int[] corpFlightBookings(int[][] bookings, int n) {
+#         int[] arr = new int[n];
+#         for (int i = 0; i < bookings.length; i++) {
+#             int[] row = bookings[i];
+#             for (int j = row[0]; j <= row[1]; j++) {
+#                 arr[j - 1] = arr[j - 1] + row[2];
+#             }
+#         }
+#         return arr;
+#     }
+# }
