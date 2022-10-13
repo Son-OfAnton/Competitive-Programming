cdd126e83022fe626fa1e60846669422e810b3e8
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-13 15:55:32 +0300

    Update boatsToSavePeople.py

--- LeetCode/boatsToSavePeople.py
+++ LeetCode/boatsToSavePeople.py
@@ -2,19 +2,19 @@
 
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        number_of_boats = 0
+        boatCount = 0
         i = 0
         j = len(people) - 1
         people.sort()
 
         while i <= j:
-            remain = limit - people[j]
+            diff = limit - people[j]
             j -= 1
-            if people[i] <= remain:
+            if people[i] <= diff:
                 i += 1
-            number_of_boats += 1
+            boatCount += 1
 
-        return number_of_boats
+        return boatCount
 
 # After sorting it will be easier to spot the heaviest 
 # and the lighest person. Then we will check the possibility 
