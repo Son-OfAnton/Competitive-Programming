f7b535866815eec2b3ffe3c187fec139e151ef3c
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-21 14:31:04 +0300

    Create findOriginalArrayFromDoubledArray.py

---
+++ LeetCode/findOriginalArrayFromDoubledArray.py
@@ -0,0 +1,22 @@
+# 2007. Find Original Array From Doubled Array
+
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        num_freq = Counter(changed)
+        changed.sort()
+        original = []
+        
+        for num in changed:
+            if num_freq[num] > 0:
+                num_freq[num] -= 1
+                if num_freq[num * 2] > 0:
+                    num_freq[num * 2] -= 1
+                    original.append(num)
+                else:
+                    return []
+        return original
+    
+# If the array is a doubled array we can keep deleting 
+# an element and its double and end up with an empty array.
+    
+    
