f5f28cc6ebb03c0971b2e88eeb92bb4555210ff7
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-10-25 20:15:30 +0300

    Create jumpGameVI.py

---
+++ LeetCode/jumpGameVI.py
@@ -0,0 +1,21 @@
+# 1696. Jump Game VI
+
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        queue = deque()
+        queue.append(0)
+        
+        for i in range(1, len(nums)):
+            while queue and queue[0] < i - k:
+                queue.popleft()
+                
+            nums[i] += nums[queue[0]]
+            
+            while queue and nums[i] >= nums[queue[-1]]:
+                queue.pop()
+            
+            queue.append(i)
+        
+        return nums[-1]
+
+
