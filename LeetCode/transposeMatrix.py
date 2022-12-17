f55dff419c29d615519018190d11bc9555ed854c
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-12-17 14:05:58 +0300

    Update transposeMatrix.py

--- LeetCode/transposeMatrix.py
+++ LeetCode/transposeMatrix.py
@@ -6,12 +6,12 @@ class Solution:
         n_rows = len(matrix)
         n_columns = len(matrix[0])
 
-        for i in range(n_columns):
+        for col in range(n_columns):
             resMatrix.append([0] * n_rows)
 
-        for i in range(n_rows):
-            for j in range(n_columns):
-                resMatrix[j][i] = matrix[i][j]
+        for row in range(n_rows):
+            for col in range(n_columns):
+                resMatrix[col][row] = matrix[row][col]
 
         return resMatrix
 
