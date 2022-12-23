2106f0ec7bda9a1c69fbfe92e426331df7b02607
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-12-23 23:44:52 +0300

    Create multiplyStrings.py

---
+++ LeetCode/multiplyStrings.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/multiply-strings/
+
+class Solution:
+    def toStringParser(self, char_digit: chr) -> int:
+        char_int = {}
+        
+        for digit in range(10):
+            str_form = str(digit)
+            char_int[str_form] = char_int.get(str_form, 0) + digit
+        
+        return char_int[char_digit]
+        
+    def multiply(self, num1: str, num2: str) -> str:
+        int_num1 = 0
+        power = 0
+        
+        for char in reversed(num1):
+            int_num1 += self.toStringParser(char) * 10**power
+            power += 1
+            
+        int_num2 = 0
+        power = 0
+        
+        for char in reversed(num2):
+            int_num2 += self.toStringParser(char) * 10**power
+            power += 1
+            
+        product = int_num1 * int_num2
+        
+        return str(product)
+        
+        
