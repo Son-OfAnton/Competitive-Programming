6b8362baf56a34f0144292db3981da724f9102f6
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-12-23 18:19:33 +0300

    Create tupleHash.py

---
+++ HackerRank/tupleHash.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/python-tuples?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = tuple(map(int, input().split()))
+    
+    print(hash(integer_list))
