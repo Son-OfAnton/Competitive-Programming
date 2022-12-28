d6e7bab46a2947bb81504c57ccdf0dda409fb049
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-12-28 23:33:06 +0300

    Delete smallestEvenMultiple.py

--- LeetCode/smallestEvenMultiple.py
+++
@@ -1,9 +0,0 @@
-# https://leetcode.com/problems/smallest-even-multiple/
-
-class Solution:
-    def smallestEvenMultiple(self, n: int) -> int:
-        if n%2 == 0:
-            return n
-    
-        return n*2
-        
