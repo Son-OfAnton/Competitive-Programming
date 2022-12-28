d7825cda683c2f2fe0128934e4ba6fb35fd7402f
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2022-12-28 23:26:18 +0300

    Update smallestEvenMultiple.py

--- LeetCode/smallestEvenMultiple.py
+++ LeetCode/smallestEvenMultiple.py
@@ -1,12 +1,9 @@
 # https://leetcode.com/problems/smallest-even-multiple/
 
 class Solution:
-    def gcd(self, a: int, b: int) -> int:
-        if b == 0:
-            return a
-        else:
-            return self.gcd(b, a%b)
-
     def smallestEvenMultiple(self, n: int) -> int:
-        return int((2*n) / self.gcd(n, 2))
+        if n%2 == 0:
+            return n
+    
+        return n*2
         
