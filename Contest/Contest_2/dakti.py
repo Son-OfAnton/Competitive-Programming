38042aae9196a45e24ef0d86d8fdbb5ae9699494
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-02 19:34:44 +0300

    Create dakti.py

---
+++ Contest/Contest_2/dakti.py
@@ -0,0 +1,31 @@
+# https://codeforces.com/gym/419351/problem/B
+
+cases = int(input())
+
+for i in range(cases):
+    phrases = input().split()
+    
+    order = []
+    rank = -1
+
+    for i, phrase in enumerate(phrases):
+        for j, char in enumerate(phrase):
+            if char.isnumeric():
+                rank += 1
+                order.append([int(char),i])
+    
+    order.sort(key= lambda x:x[0] )    
+    for k, p in enumerate(phrases):
+        s = ""
+        for z, ch in enumerate(p):
+            if ch.isnumeric():
+                continue
+            else:
+                s += ch
+        phrases[k] = s 
+
+    res = ""
+    for a, b in order:
+        res += (phrases[b] + " ")
+    print(res[:len(res)-1])
+    
