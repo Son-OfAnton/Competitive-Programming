aafdf61cec98876e224dbc235f5c92df8c991440
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-21 19:57:20 +0300

    Update selectionSort.py

--- GeekForGeeks/selectionSort.py
+++ GeekForGeeks/selectionSort.py
@@ -1,16 +1,8 @@
 # https://practice.geeksforgeeks.org/problems/selection-sort/1
 
 class Solution: 
-    def select(self, arr, i):
-        # code here 
-        while arr[i+1] > arr[i]:
-            i += 1
-        
-        return arr[i+1]
-    
     def selectionSort(self, arr,n):
         #code here
-
         for i in range(len(arr)):
             min = i
             for j in range(i+1, len(arr)):
