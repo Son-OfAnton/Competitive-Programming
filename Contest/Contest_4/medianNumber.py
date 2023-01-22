b0f725de5abbbbf904a447de8e4ef0a573559353
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-01-22 15:26:42 +0300

    Create medianNumber.py

---
+++ Contest/Contest_4/medianNumber.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/gym/422945/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    nums = list(map(int, input().split()))
+    nums.sort()
+    size = len(nums)
+    
+    print(nums[size//2])
+
