b67d90ea64c39c66838914e1f838cc5128432d75
Son-OfAnton <93878882+Son-OfAnton@users.noreply.github.com>
2023-03-21 00:36:21 +0300

    Create baskeballTogether.py

---
+++ Contest/Contest_12/baskeballTogether.py
@@ -0,0 +1,23 @@
+# https://codeforces.com/gym/433454/problem/A
+
+N, D = map(int, input().split())
+powers = list(map(int, input().split()))
+
+powers.sort()
+left = 0
+right = N - 1
+count = 0
+
+while left <= right:
+    summ = powers[right]
+
+    while summ <= D and left < right:
+        summ += powers[right]
+        left += 1
+
+    if summ > D:
+        count += 1
+
+    right -= 1
+
+print(count)
