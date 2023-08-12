43c78df0e48a65ba63913fbbd1fe4dd8156181bb
Son-OfAnton <admasterefe00@gmail.com>
2023-08-12 11:35:26 +0300

    cool A_Average_Height solution

--- Contest/Contest_29/A_Average_Height.py
+++ Contest/Contest_29/A_Average_Height.py
@@ -5,12 +5,13 @@ for _ in range(t):
     n = int(input())
     arr = list(map(int, input().split()))
 
-    R = 0
-    for L in range(n):
-        while R < n and arr[R] % 2 == 1:
-            R += 1
+    arr.sort(key=lambda x: x % 2 == 0)
+    # R = 0
+    # for L in range(n):
+    #     while R < n and arr[R] % 2 == 1:
+    #         R += 1
         
-        if R < n:
-            arr[L], arr[R] = arr[R], arr[L]
+    #     if R < n:
+    #         arr[L], arr[R] = arr[R], arr[L]
 
     print(*arr)
